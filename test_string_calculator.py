4bda49166237af917e51dc9734c849f35664bbf2
Shiva Kant Mishra <shivakant.mishra1995@gmail.com>
2022-03-28 00:29:37 +0530

    update test_string_calculator
    Added test case for one number input

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -6,6 +6,9 @@ class TestStringCalculator(unittest.TestCase):
     def test_add_string_empty_string_return_zero(self):
         self.assertEqual(add_string(''),0)
 
+    def test_add_string_one_number_return_zero(self):
+        self.assertEqual(add_string('5'),5)
+        self.assertEqual(add_string('10'),10)
 
 if __name__ == '__main__':
     unittest.main()
