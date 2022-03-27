6c496c033eeea509911187c54940964aaddbb024
Shiva Kant Mishra <shivakant.mishra1995@gmail.com>
2022-03-28 00:59:35 +0530

    Updated test_string_calculator.py
    added test case for n number input

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -14,6 +14,10 @@ class TestStringCalculator(unittest.TestCase):
         self.assertEqual(add_string('5,10'),15)
         self.assertEqual(add_string('22,22'),44)
     
+    def test_add_string_two_numbers_return_sum_of_n_numbers(self):
+        self.assertEqual(add_string('5,10,20'),35)
+        self.assertEqual(add_string('10,20,30,40,50'),150)
+    
 
 if __name__ == '__main__':
     unittest.main()
