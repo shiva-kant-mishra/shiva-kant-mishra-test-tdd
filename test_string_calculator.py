c0359818f9b1cb0a7d002bc0c8b904da13d65fcf
Shiva Kant Mishra <shivakant.mishra1995@gmail.com>
2022-03-28 00:40:59 +0530

    Updated test_string_calculator.py
    added test case for two number input

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -6,9 +6,14 @@ class TestStringCalculator(unittest.TestCase):
     def test_add_string_empty_string_return_zero(self):
         self.assertEqual(add_string(''),0)
 
-    def test_add_string_one_number_return_zero(self):
+    def test_add_string_one_number_return_number(self):
         self.assertEqual(add_string('5'),5)
         self.assertEqual(add_string('10'),10)
+    
+    def test_add_string_two_numbers_return_sum(self):
+        self.assertEqual(add_string('5,10'),15)
+        self.assertEqual(add_string('22,22'),44)
+    
 
 if __name__ == '__main__':
     unittest.main()
