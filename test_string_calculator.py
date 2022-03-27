ee3a67708609786ee8ee55d90ea4e0cc6954150a
Shiva Kant Mishra <shivakant.mishra1995@gmail.com>
2022-03-28 00:17:48 +0530

    Updates test_string_calculator.py
    removed class as not part of requirement

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -1,11 +1,10 @@
 import unittest
-from string_calculator import StringCalculator
+from string_calculator import add_string
 
 class TestStringCalculator(unittest.TestCase):
 
     def test_add_string_empty_string_return_zero(self):
-        sc = StringCalculator()
-        self.assertEqual(sc.add_string(''),0)
+        self.assertEqual(add_string(''),0)
 
 
 if __name__ == '__main__':
