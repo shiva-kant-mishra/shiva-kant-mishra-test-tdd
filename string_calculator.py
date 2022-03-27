f748c40d33e220598c62a4d3aafcb448d6d4b5c9
Shiva Kant Mishra <shivakant.mishra1995@gmail.com>
2022-03-28 01:14:05 +0530

    Updated string_calculator.py
    Added code to handle newline between numbers instead of comma

--- string_calculator.py
+++ string_calculator.py
@@ -1,11 +1,14 @@
 def add_string(string_numbers):
     if len(string_numbers)==0:
         return 0
-    elif len(string_numbers.split(","))==1:
-        return int(string_numbers)
+    
     else:
         numbers_after_split = string_numbers.split(",")
         result = 0
         for number in numbers_after_split:
-            result += int(number)
+            if len(number.split("\n"))>1:
+                newline_split_number = number.split('\n')
+                result += int(newline_split_number[0]) + int(newline_split_number[1])
+            else:
+                result += int(number)
         return result
