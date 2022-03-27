0ebea60393346f4bc019b7752a4afd023b6c6c75
Shiva Kant Mishra <shivakant.mishra1995@gmail.com>
2022-03-28 00:49:29 +0530

    Updated string_calculator.py
    added code to handle two number input

--- string_calculator.py
+++ string_calculator.py
@@ -1,5 +1,8 @@
 def add_string(string_numbers):
     if len(string_numbers)==0:
         return 0
-    else:
+    elif len(string_numbers.split(","))==1:
         return int(string_numbers)
+    else:
+        numbers_after_split = string_numbers.split(",")
+        return int(numbers_after_split[0]) + int(numbers_after_split[1])
